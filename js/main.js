43357a6030e24e80621676b3362fdd0dd28dcfa4
sunnysideup0w0 <sunnysideup0w0@gmail.com>
2020-05-20 22:02:26 +0900

    delete All List button

--- js/main.js
+++ js/main.js
@@ -1,25 +1,52 @@
 let btnSubmit = document.querySelector(".addList button");
 let input = document.querySelector(".addList input");
-// let deleteLi = document.querySelectorAll(".itemList li i");
 let itemList = document.querySelector(".itemList ul");
 let itemLi = document.querySelectorAll(".itemList ul li")
-let clearAll = document.querySelector(".itemList button");
+let clearWillBuy = document.querySelector(".itemList button");
+let basketList = document.querySelector(".inBasket ul");
+let clearAllList = document.querySelector(".bg button")
 
-console.log(itemLi)
-//console.log(deleteLi)
+
+// function remove(){
+//     let deleteLi = document.querySelectorAll(".itemList li i");
+//     deleteLi.forEach(function(i){
+//         i.addEventListener("click",function(e){
+//             console.log(i)
+//            if(e.target.tagName.toLowerCase() === 'i'){
+//                 i.parentElement.remove();
+//            }
+//         });
+//     })
+// }
+// remove();
+
+function check(){
+    itemLi = document.querySelectorAll(".itemList ul li")
+    if(itemLi.length === 0){
+        clearWillBuy.classList.add("none")
+    } else {
+        clearWillBuy.classList.remove("none")
+    }
+}
+
+check();
 
 function addList(){
     let value = input.value;
     let node = document.createElement("li");
-    let li = `${value} <i class="xi-trash" onclick="remove();"></i>`
+    let li = `${value} <i class="xi-trash""></i>`
     node.innerHTML = li;
     node.children[0].addEventListener("click",function(e){
         if(e.target.tagName.toLowerCase() === 'i'){
             node.children[0].parentElement.remove();
+            node.children[0].remove();
+            basketList.appendChild(node);
         }
+        check();
     });
     itemList.appendChild(node);
     input.value = "";
+    check();
 }
 
 input.addEventListener("keydown",function(){
@@ -28,24 +55,23 @@ input.addEventListener("keydown",function(){
     }
 })
 
-function remove(){
-    let deleteLi = document.querySelectorAll(".itemList li i");
-    deleteLi.forEach(function(i){
-        i.addEventListener("click",function(e){
-           if(e.target.tagName.toLowerCase() === 'i'){
-                i.parentElement.remove();
-           }
-        });
-    })
-}
-
-remove();
-
 // dynamic element cannot listen event.
 
-clearAll.addEventListener("click",function(){
-    itemLi = document.querySelectorAll(".itemList ul li")
+clearWillBuy.addEventListener("click",function(){
     itemLi.forEach(function(li){
         li.remove();
     })
+    check();
+})
+
+clearAllList.addEventListener("click",function(){
+    itemLi = document.querySelectorAll(".itemList ul li");
+    let basketLi = document.querySelectorAll(".inBasket ul li");
+    itemLi.forEach(function(li){
+        li.remove();
+    })
+    basketLi.forEach(function(li){
+        li.remove();
+    })
+    check();
 })
